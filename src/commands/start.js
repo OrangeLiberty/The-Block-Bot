b8af0a9fb107b4e793f468976d53d1824230e6db
Dev_Dennis <98164069+OrangeLiberty@users.noreply.github.com>
2023-01-04 14:33:34 +0100

    fix feedback link

--- src/commands/start.js
+++ src/commands/start.js
@@ -3,7 +3,7 @@ module.exports = (bot) => {
     bot.command("start", async(ctx) => {
         let userName = ctx.message.from.first_name;
         let startMessage = `👋Welcome ${userName}!\n\n🔍This Block Explorer provides you with Information about the Bitcoin Blockchain.\n\n
-🫵 You can check:\n\n🚦 The current transaction traffic\n🧮 How high is the hashrate?\n🤷🏽‍♂️ What is the recommended fee?\n⏳ What Blocktime is it?\n👀 Is your transaction already confirmed?\n🚀 And much more...\n\n⚙️ Commands:\n\n/start 📚 Start from the beginning.\n/explorer 🔭 Discover the Mempool.\n/help 📖 Read this help.\n/tip 🧡 Support the work.\n\n👋 Join us for feedback on https://t.me/+SZAq0Vxjmrk1NDdi
+🫵 You can check:\n\n🚦 The current transaction traffic\n🧮 How high is the hashrate?\n🤷🏽‍♂️ What is the recommended fee?\n⏳ What Blocktime is it?\n👀 Is your transaction already confirmed?\n🚀 And much more...\n\n⚙️ Commands:\n\n/start 📚 Start from the beginning.\n/explorer 🔭 Discover the Mempool.\n/help 📖 Read this help.\n/tip 🧡 Support the work.\n\n👋 Join us for feedback on https://t.me/+RKtnoB4n1ys0ZWNi
 `;
         await ctx.reply(startMessage);
         console.log(
